61d7eca62d6ad348dfe7415e3606065faaf1199f
cana <cana@localhost.localdomain>
2015-02-08 10:51:11 -0500

    commit first source

---
+++ plot1.R
@@ -0,0 +1,13 @@
+if (!"load_data.R" %in% list.files()) {
+    setwd("/home/cana/rworkspace/ExData_Plotting1/")
+}
+source("load_data.R")
+png(filename = "plot1.png",
+	width = 480, height = 480,
+	units = "px", bg = "transparent")
+hist(Global_active_power,
+	col = "red",
+	main = "Global Active Power",
+	xlab = "Global Active Power (kilowatts)",
+	breaks = 12, ylim = c(0, 1200))
+    dev.off()
